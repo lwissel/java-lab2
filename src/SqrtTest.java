ea94293b48cabdf423ac754813637deb734ca55b
lwissel <lennart.wissel@gmx.net>
2014-10-10 10:12:48 +0100

    nextstage test

--- src/SqrtTest.java
+++ src/SqrtTest.java
@@ -4,6 +4,7 @@ public class SqrtTest{
 
   @Test
     public void sqrtTest1() {
-      assertEquals
+      assertEquals("We test whether the computation of sqrt(4) is correct",
+                    2, Math.sqrt(4), 0.000001);
     }
 }
